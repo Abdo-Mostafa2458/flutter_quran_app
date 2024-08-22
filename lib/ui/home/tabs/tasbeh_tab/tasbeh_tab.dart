2c18d1a6191772d3f12da93c6f710ef02e03b926
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-22 23:52:58 +0300

    + Update tasbeh_tab.dart + add images

--- lib/ui/home/tabs/tasbeh_tab/tasbeh_tab.dart
+++ lib/ui/home/tabs/tasbeh_tab/tasbeh_tab.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:islami_c11_friday/utils/image_utils.dart';
 
-class TasbehTab extends StatefulWidget {
+class TasbehTab extends StatelessWidget {
   const TasbehTab({super.key});
-
-  @override
-  State<TasbehTab> createState() => _TasbehTabState();
-}
-
-class _TasbehTabState extends State<TasbehTab> {
-  int counter = 0;
-  double angelConunt = 0;
-  List<String> Azkarr = [
-    "سبحان الله",
-    "الحمد لله",
-    "لا اله الا الله",
-    "الله اكبر",
-  ];
-  int lableText = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,16 +19,8 @@ class _TasbehTabState extends State<TasbehTab> {
             ),
             Padding(
               padding: EdgeInsets.only(top: 50),
-              child: Transform.rotate(
-                angle: angelConunt,
-                child: GestureDetector(
-                  onTap: () {
-                    OnTap();
-                  },
-                  child: Image.asset(
-                      getImagePathByName(imageName: 'body of seb7a_light.png')),
-                ),
-              ),
+              child: Image.asset(
+                  getImagePathByName(imageName: 'body of seb7a_light.png')),
             ),
           ],
         ),
@@ -52,50 +29,32 @@ class _TasbehTabState extends State<TasbehTab> {
           child: Text(
             "عدد التسبيحات",
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.labelMedium,
+            style: Theme.of(context).textTheme.displaySmall,
           ),
         ),
-        Padding(
-          padding: EdgeInsets.only(top: 27),
-          child: Center(
-            child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(30)),
-              child: Text("$counter",
-                  style: Theme.of(context).textTheme.labelMedium),
+        Padding(padding: EdgeInsets.only(top: 27),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor.withOpacity(0.7),
+              borderRadius: BorderRadius.circular(30)
             ),
+            child: Text("33",style: Theme.of(context).textTheme.labelMedium),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 15),
-          child: Center(
-            child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Text("${Azkarr[lableText]}",
-                  style: Theme.of(context).textTheme.labelMedium),
+        ),),
+        Padding(padding: EdgeInsets.only(top: 15),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30)
             ),
+            child: Text("سبحان الله",style: Theme.of(context).textTheme.labelMedium),
           ),
-        ),
+        ),),
       ],
     );
-  }
-
-  OnTap() {
-    counter++;
-    if (counter % 33 == 0) {
-      lableText++;
-      //counter++;
-      counter = 0;
-    }
-    if (lableText == Azkarr.length) {
-      lableText = 0;
-    }
-    angelConunt += 360 / 4;
-    setState(() {});
   }
 }
